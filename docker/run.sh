d957b71f60f7d21922edfe39b61e895772e97870
Anton Sherkhonov <asherkho@redhat.com>
2016-11-30 17:20:27 +0000

    changed Logstash version to 5.0.x

--- docker/run.sh
+++ docker/run.sh
@@ -4,10 +4,10 @@ set -e
 set -x
 
 if [ "$DEBUG_LOGSTASH" = true ]; then
-    LOGSTASH_ARGS="--debug"
+    LOGSTASH_ARGS="--config.debug --log.level trace"
 else
     LOGSTASH_ARGS=""
 fi
 
-/opt/logstash/bin/logstash agent ${LOGSTASH_ARGS} --config /etc/logstash/conf.d/
+/usr/share/logstash/bin/logstash ${LOGSTASH_ARGS} --log.format json --path.config /etc/logstash/conf.d/ --path.data /opt/app-root/
 
